04abfd57e7ac271f316ee41c6b6447e2005549f1
thmichaellin <thmichaellin@gmail.com>
2024-10-29 17:41:41 +0100

    Submission
    Fixed grade_task2.py
    Completed tasks.py

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 
 # Follow the tasks below to practice basic Python concepts.
 # Write your code in between the dashed lines.
@@ -13,8 +13,20 @@ import numpy
 # Your code here:
 # -----------------------------------------------
 
-def step
+def step(input: float) -> int:
+    """
+    Return 1 if input is larger than 0; else return -1.
 
+    Args:
+        input (float): Input number.
+
+    Returns:
+        int: 1 if input > 0; else -1
+    """
+
+    if input > 0:
+        return 1
+    return -1
 
 # -----------------------------------------------
 
@@ -28,8 +40,21 @@ def step
 
 # Your code here:
 # -----------------------------------------------
-def ReLu
+def ReLu(array_in: np.array, cutoff: int = 0) -> np.array:
+    """
+    Take numpy array and return numpy array of same length where all elements < cutoff are replaced
+    with the cutoff value.
 
+    Args:
+        array (np.array): Input array.
+        cutoff (int, optional): Cutoff value. Defaults to 0.
+
+    Returns:
+        np.array: Output array.
+    """
+
+    array_in[array_in < cutoff] = cutoff
+    return array
 
 # -----------------------------------------------
 
@@ -44,7 +69,26 @@ def ReLu
 # Your code here:
 # -----------------------------------------------
 
-def neural_net_layer
+def neural_net_layer(array_in_2d: np.array, array_in_1d: np.array) -> np.array:
+    """
+    Find product of an (n, p) matrix and (p) matrix.
+    Apply ReLu activation to the resulting matrix.
+
+    Args:
+        array_in_2d (np.array): 2D input array.
+        array_in_1d (np.array): 1D input array.
+
+    Raises:
+        ValueError: If dimensions of matrices do not match.
+
+    Returns:
+        np.array: Output array.
+    """
+    if not array_in_2d.shape[1] == array_in_1d.shape[0]:
+        raise ValueError("Input arrays should have shapes (n, p) and (p)")
+    array_mult = array_in_2d.matmul(array_in_1d)
+    return ReLu(array_mult)
+
 
 
 # ------------------------------------------
